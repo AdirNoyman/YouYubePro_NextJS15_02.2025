047d4b4642fc64018a233e7e981e50235a150741
Adir Noyman <ofahol2007@hotmail.com>
2025-02-15 14:03:57 +0200

    .

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex items-center gap-2">
-      <Image src="/logo.svg" alt="logo" width={50} height={50} />
-      <p className='text-xl font-semibold tracking-tight'>New Tube</p>
+    <div>
+      Here I&apos;ll put all my videos
     </div>
   );
 }
--- src/modules/home/ui/components/home-sidebar/index.tsx
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -1,12 +1,16 @@
 import { Sidebar, SidebarContent } from '@/components/ui/sidebar'
 import React from 'react'
 import SideBarMainSection from './main-section'
+import { Separator } from '@/components/ui/separator'
+import SideBarPersonalSection from './personal-section'
 
 const HomeSidebar = () => {
   return (
-    <Sidebar className='pt-16 z-40 border-none'>
+    <Sidebar className='pt-16 z-40 border-none' collapsible='icon'>
         <SidebarContent className='bg-background'>
             <SideBarMainSection />
+            <Separator />
+            <SideBarPersonalSection />
         </SidebarContent>
     </Sidebar>
   )
